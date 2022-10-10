ecdc872bda1775bdbc82779e926ecb60c85a982e
Daryl <qingyu.tan.2020@sis.smu.edu.sg>
2022-10-10 23:02:30 +0800

    Initial change.

---
+++ Components/StocksList/StocksList.js
@@ -0,0 +1,49 @@
+// Stocks Component
+app.component("stockslist", {
+  data() {
+    return {
+      stockList: [
+        {
+          Symbol: "TSLA",
+          Price: "$388",
+          Change: "$122",
+          ChangePercent: "30%",
+          Volume: "39000000",
+        },
+      ],
+      stockTitle: ["Symbol", "Price", "Change", "Change %", "Volume"],
+    };
+  },
+  computed: {
+    stocks() {
+      //   let response = axios
+      //     .get("http://localhost:8000/api/stocks")
+      //     .then((response) => {
+      //       this.stockList = response.data;
+      //     });
+
+      return this.stockList;
+    },
+  },
+  template: `
+    <h3 class="my-5 mx-auto" v-if="typeof stockList == null">Ooops.. there is no stocks now...</h3>
+    <table class="table" v-else>
+  <thead>
+    <tr v-for="stock of stockTitle">
+      <th scope="col">{{stock.title}}</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr v-for="listings of stockList">
+      <th scope="row">{{listings.Symbol}}</th>
+      <td>{{listings.Price}}</td>
+        <td>{{listings.Change}}</td>
+        <td>{{listings.ChangePercent}}</td>
+        <td>{{listings.Volume}}</td>
+    </tr>
+  </tbody>
+  </table>
+        `,
+});
+
+app.mount("#stockslist");
