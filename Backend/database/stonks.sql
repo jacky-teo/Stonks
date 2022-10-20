266d09642d0a8411fda871e487434532a5cb55cd
jacky-teo <jackytjq@outlook.com>
2022-10-21 00:00:56 +0800

    Fixed Database,,,

--- Backend/database/stonks.sql
+++ Backend/database/stonks.sql
@@ -36,15 +36,16 @@ INSERT INTO `funds` (`fund_id`, `fund_name`, `fund_goals`,`fund_investment_amoun
 -- Stocks available for trade with Stonks -- 
 DROP TABLE IF EXISTS `stocks`;
 CREATE TABLE IF NOT EXISTS `stocks` (
+	`stock_id` int not null,
   `stock_symbol` varchar (10) not null,
   `stock_name` varchar(50) NOT NULL,
-  PRIMARY KEY (`stock_symbol`)
+  PRIMARY KEY (`stock_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-INSERT INTO `stocks` (`stock_name`, `stock_symbol`) VALUES
-('YZJ Shipbldg SGD', 'BS6.SI'),
-('Singtel', 'Z74.SI'),
-('DBS', 'D05.SI');
+INSERT INTO `stocks` (`stock_id`,`stock_name`, `stock_symbol`) VALUES
+(1,'YZJ Shipbldg SGD', 'BS6.SI'),
+(2,'Singtel', 'Z74.SI'),
+(3,'DBS', 'D05.SI');
 
 
 -- Stocks owned by a customer -- 
@@ -53,20 +54,20 @@ DROP TABLE IF EXISTS `settlements`;
 CREATE TABLE IF NOT EXISTS `settlements`(
     `settlement_id` int not Null,
 	`user_id` int NOT NULL,
-    `stock_symbol` varchar(50) NOT NULL,
+    `stock_id` int NOT NULL,
     `stock_price` float not Null,
     `volume` int not Null,
 	 PRIMARY KEY (settlement_id),
      FOREIGN KEY (`user_id`) REFERENCES users(`user_id`),
-	 FOREIGN KEY (`stock_symbol`) REFERENCES stocks(`stock_symbol`)
+	 FOREIGN KEY (`stock_id`) REFERENCES stocks(`stock_id`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
 
-INSERT INTO `settlements` (`settlement_id`, `user_id`, `stock_symbol`,`stock_price`,`volume`) VALUES
-(1,1, 'BS6.SI', 0.5, 1000),
-(2,1, 'Z74.SI', 3.5, 1000),
-(3,1, 'D05.SI', 22.5, 1000);    
+INSERT INTO `settlements` (`settlement_id`, `user_id`, `stock_id`,`stock_price`,`volume`) VALUES
+(1,1, 1, 0.5, 1000),
+(2,1, 2, 3.5, 1000),
+(3,1, 3, 22.5, 1000);    
 
 
 
@@ -102,6 +103,7 @@ INSERT INTO `users_funds` (`user_id`, `fund_id`) VALUES
 
 
 
+
 -- Stocks available in the marketplace this will act as the CDP -- 
 DROP TABLE IF EXISTS `marketplace`;
 CREATE TABLE IF NOT EXISTS `marketplace`(
@@ -119,17 +121,17 @@ INSERT INTO `marketplace` (`marketplace_id`, `marketplace_name`) VALUES
 DROP TABLE IF EXISTS `marketplace_stocks`;
 CREATE TABLE IF NOT EXISTS `marketplace_stocks`(
 	`marketplace_id` int not Null,
-    `stock_symbol` varchar(50) NOT NULL,
+    `stock_id` int NOT NULL,
 	`volume_in_market` int Not Null,
-    PRIMARY KEY (`marketplace_id`,`stock_symbol`),
+    PRIMARY KEY (`marketplace_id`,`stock_id`),
     FOREIGN KEY (`marketplace_id`) REFERENCES marketplace(`marketplace_id`),
-	FOREIGN KEY (`stock_symbol`) REFERENCES stocks(`stock_symbol`)
+	FOREIGN KEY (`stock_id`) REFERENCES stocks(`stock_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-INSERT INTO `marketplace_stocks` (`marketplace_id`, `stock_symbol`,`volume_in_market`) VALUES
-(1,'BS6.SI',1000000),
-(1,'Z74.SI',1000000),
-(1,'D05.SI',1000000);
+INSERT INTO `marketplace_stocks` (`marketplace_id`, `stock_id`,`volume_in_market`) VALUES
+(1,1,1000000),
+(1,2,1000000),
+(1,3,1000000);
 
 
 -- This will be an insert only table -- 
@@ -139,20 +141,21 @@ CREATE TABLE IF NOT EXISTS `transactions`(
     `transaction_id` int not Null,
 	`user_id` int NOT NULL,
     `marketplace_id` int NOT NULL,
-    `stock_symbol` varchar(50) NOT NULL,
+    `stock_id` int NOT NULL,
     `stock_price` float not Null,
     `volume` int not Null,
     `date` datetime not null,
 	PRIMARY KEY (transaction_id),
     FOREIGN KEY (`user_id`) REFERENCES users(`user_id`),
-	FOREIGN KEY (`stock_symbol`) REFERENCES stocks(`stock_symbol`),
+	FOREIGN KEY (`stock_id`) REFERENCES stocks(`stock_id`),
     FOREIGN KEY (`marketplace_id`) REFERENCES marketplace(`marketplace_id`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
 -- all transactions with marketplace will be reflected here -- 
-INSERT INTO `transactions` (`transaction_id`, `user_id`,`marketplace_id`, `stock_symbol`,`stock_price`,`volume`,`date`) VALUES
-(1,1, 1,'BS6.SI', 0.5, 1000, '2020-01-01 00:00:00'),
-(2,1, 1,'Z74.SI', 3.5, -1000, '2020-01-01 00:00:00'),
-(3,1, 1,'D05.SI', 22.5, 1000, '2020-01-01 00:00:00');
+INSERT INTO `transactions` (`transaction_id`, `user_id`,`marketplace_id`, `stock_id`,`stock_price`,`volume`,`date`) VALUES
+(1,1, 1,1, 0.5, 1000, '2020-01-01 00:00:00'),
+(2,1, 1,2, 3.5, -1000, '2020-01-01 00:00:00'),
+(3,1, 1,3, 22.5, 1000, '2020-01-01 00:00:00'),
+(4,2, 1,3, 22.5, 1000, '2020-01-01 00:00:00');
 
