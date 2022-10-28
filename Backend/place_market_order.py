2d1561d96c416f5ba74ed1d4e0a3a5395a9178d5
Vasn <vasilisngjunquan@gmail.com>
2022-10-29 03:44:36 +0800

    refactor

--- Backend/place_market_order.py
+++ Backend/place_market_order.py
@@ -4,6 +4,7 @@ import ast
 
 import requests, json # for api requests
 from functions import url, getRecord # tbank gateway endpoint
+from placeMarketOrder import placeMarketOrder
 from getCustomerStocks import getStockSymbols
 
 
@@ -77,53 +78,6 @@ def process_rebalance(additional_invest, allocation, price, current_shares, user
     # getCustomerStocks(): Get all stocks owned by customer (including 0 quantity)
 
 # ====================
-def placeMarketOrder(userID, PIN, settlementAccount, buyOrSell, symbol, quantity, OTP):
-    '''
-        Takes in the following inputs:
-            - userID                [String]
-            - PIN                   [String]
-            - settlementAccount     [String]
-            - buyOrSell             [String]
-            - symbol                [String]
-            - quantity              [String]
-            - OTP                   [String]
-        and outputs:
-            - None                  [NoneType]
-    '''
-    #Header
-    serviceName = 'placeMarketOrder'
-    
-    headerObj = {
-        'Header': {
-            'serviceName': serviceName,
-            'userID': userID,
-            'PIN': PIN,
-            'OTP': OTP
-        }
-    }
-    contentObj = {
-        'Content': {
-            'settlementAccount': settlementAccount,
-            'symbol': symbol,
-            'buyOrSell': buyOrSell,
-            'quantity': quantity
-        }
-    }
-    final_url="{0}?Header={1}&Content={2}".format(url(),json.dumps(headerObj),json.dumps(contentObj))
-    response = requests.post(final_url)
-    serviceRespHeader = response.json()['Content']['ServiceResponse']['ServiceRespHeader']
-    errorCode = serviceRespHeader['GlobalErrorID']
-    
-    if errorCode == '010000':
-        marketOrder = response.json()['Content']['ServiceResponse']['StockOrder']
-        return "You have successfully placed a market order. The order ID is {}.".format(marketOrder['orderID'])
-
-    elif errorCode == '010041':
-        return "OTP has expired.\nYou will receiving a SMS"
-    else:
-        return serviceRespHeader['ErrorText']
-
-
 def getCustomerStocks(userID, PIN, OTP):
     #Header
     serviceName = 'getCustomerStocks'
