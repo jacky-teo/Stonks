afd20b8461d356e1f7a8aa4f7bca2dfe6756332d
weeht1996 <72547656+weeht1996@users.noreply.github.com>
2022-11-07 02:31:35 +0800

    StockHistory Chart (pre-integration)

---
+++ Components/FundStockHistory/FundStockHistory.js
@@ -0,0 +1,13 @@
+const fundstockhistory = Vue.createApp({
+  template: `
+  <div class="shadow-lg p-3 mb-5 bg-white rounded">
+    <h1 class="text-center">Stock History</h1>
+    <div class="row d-flex justify-content-center align-content-center">
+      <canvas id="linechart" style="width:100%;max-width:700px"></canvas>
+    </div>
+    <div class="row d-flex justify-content-center align-content-center m-3">
+    </div>
+  </div>`
+})
+fundstockhistory.mount('#fundstockhistory')
+
