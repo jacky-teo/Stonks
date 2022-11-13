2cd3f9a23db8bdaad9643411154a218d784674bc
Vasn <vasilisngjunquan@gmail.com>
2022-11-14 01:42:58 +0800

    updated rebalancing

--- Components/CurrentPieChart/CurrentFundPieChart.js
+++ Components/CurrentPieChart/CurrentFundPieChart.js
@@ -31,14 +31,107 @@ const currentfundpiechart = Vue.createApp({
 					console.log(error);
 				});
 		},
-        redirectBalance(){
-            window.location.href = "rebalance.html?fund_id=" + this.fundId;
+        placeMarketOrder(tBankUID, tBankPin, tBankSettlementAccount, allocation){
+            var data = {
+				"userID": tBankUID,
+				"PIN": tBankPin, 
+				"additionalInvest": 0,
+				"allocation": JSON.stringify(allocation),
+				"settlement_account": tBankSettlementAccount,
+			}
+			return new Promise((resolve, reject) => {
+				axios.post("http://localhost:5010/rebalance", data).then((response) => {
+					resolve(response);
+				}).catch(error => {
+					reject(error);
+				});
+			})
+        },
+		returnAllocationsBasedOnPlaceMarketOrderFormat(items) {
+			var allocations = {}
+			items.forEach((item) => {
+				allocations[item.stock_symbol] = (item.stock_allocation/100)
+			})
+			return allocations
+		},
+		getFundStocks() {
+            return new Promise((resolve, reject) => {
+                axios.get('http://localhost:5001/a_fund_stocks/' + this.fundId)
+                .then((response) => {
+                    var data = response.data.data.allocation
+					var allocations = {}
+					data.forEach((item) => {
+						allocations[item.stock_id] = item.allocation
+					})
+                    resolve(allocations)
+                }).catch((error) => {
+                    reject(error)
+                })
+            })
+        },
+        getStocks() {
+            return new Promise((resolve, reject) => {
+                axios.get('http://localhost:5003/stocks')
+                .then((response) => {
+                    var data = response.data.data.stocks
+                    var stocks = {}
+                    for (stock of data) {
+                        var stockId = stock.stock_id
+                        var stockName = stock.stock_name
+                        var stockSymbol = stock.stock_symbol
+                        stocks[stockId] = {
+                            "stock_name": stockName,
+                            "stock_symbol": stockSymbol
+                        }
+                    }
+                    // console.log("getStocks", stocks)
+                    resolve(stocks)
+                }).catch((error) => {
+                    reject(error)
+                })
+            })
+        },
+		getUser() {
+            return new Promise((resolve, reject) => {
+                axios.get('http://localhost:5005/user_info/user/' + this.userId)
+                .then((response) => {
+                    var data = response.data.data
+                    console.log(data)
+                    resolve(data)
+                }).catch((error) => {
+                    reject(error)
+                })
+            })
+        },
+        async rebalance(){
+			var user = await this.getUser();
+			var response = await this.placeMarketOrder(user.user_acc_id, user.user_pin, user.settlement_acc, this.allocations);
+			
+			if (response.data.code == 200) {
+                Swal.fire({icon: 'success',title: 'Success',text: 'Fund successfully rebalanced!'}).then((result) => {
+                    if (result.isConfirmed) {
+                        window.location.href = "mystocks.html"
+                    }
+                })
+            } else {
+                Swal.fire({icon: 'error',title: 'Note',text: 'Something went wrong while rebalancing, please try again later.'})
+            }
         }
   	},
     async created() {
         await this.getUsersFunds();
         this.userId = sessionStorage.getItem("user_id");
         this.fundId = sessionStorage.getItem("fund_id");
+
+		var allocations = await this.getFundStocks();
+		var allStocks = await this.getStocks();
+
+		var cleanedAllocations = {};
+		for (key in allocations) {
+			cleanedAllocations[allStocks[key]["stock_symbol"]] = allocations[key]
+		}
+
+		this.allocations = cleanedAllocations
     },
 	template: `
 	<div class="shadow-lg p-3 mb-5 bg-white rounded">
@@ -67,9 +160,29 @@ const currentfundpiechart = Vue.createApp({
 				</tr>
 				</tbody>
 			</table>
-            <button type="button" class="btn btn-success float-end" @click="
-            redirectBalance()">Rebalance Fund</button>
+            <button type="button" class="btn btn-success float-end" data-bs-toggle="modal" data-bs-target="#comfimationModal">Rebalance Fund</button>
 		</div>
+
+		<div class="modal fade" id="comfimationModal" tabindex="-1" aria-labelledby="confirmationModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-lg modal-dialog-centered modal-dialog-scrollable">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-4" id="confirmationModalLabel">Confirm rebalance current fund to target allocation?</h1>
+                    </div>
+                    <div class="modal-body">
+                        <div class="row align-items-center">
+                            <div class="">
+								Are you sure you would like to rebalance your current fund?
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-success" @click="rebalance()">Confirm</button>
+                    </div>
+                </div>
+            </div>
+        </div>
 
 	</div>`
 })
